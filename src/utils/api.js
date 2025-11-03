cd9cc0bdad6e9f5200cf0b5e496cefb26ad51c65
Kamesh R <rkameshraj7@gmail.com>
2025-11-03 17:19:43 +0530

    Change API base URL to production endpoint
    start commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://hotel-management-backend-4hff.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
